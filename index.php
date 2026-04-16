3795c8d8ea631d460ffdc2cf8ade7f9cd8f0146a
Dar3s <danielzoubek97@gmail.com>
2026-04-16 08:25:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/config/db.php';
+require_once __DIR__ . '/init_database.php';
 
 $stmt = $pdo->query("SELECT * FROM drops ORDER BY release_date ASC");
 $drops = $stmt->fetchAll();
